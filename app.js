ca9f52be0f7e5dbff0579f851aa8f3b89694ad4e
Lucas <garcia.lucas3110@icloudd.com>
2023-07-08 03:04:07 -0400

    removed shapes because it was buggy and took too long to load

--- app.js
+++ app.js
@@ -33,36 +33,6 @@ const header = document.querySelector('header');
 header.appendChild(responsiveMenu);
 
 
-//Shapes
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-// generate random shape
-function generateShape() {
-    const container = document.getElementById('container');
-    const shapeTypes = ['square', 'triangle', 'circle'];
-    const shapeType = shapeTypes[getRandomNumber(0, 2)];
-    const shape = document.createElement('div');
-    shape.classList.add('shape', shapeType);
-
-    const maxX = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
-    const maxY = Math.max(document.documentElement.scrollWidth, document.body.scrollWidth);
-
-    const x = getRandomNumber(0, maxX);
-    const y = getRandomNumber(0, maxY);
-
-    shape.style.left = x + 'px';
-    shape.style.top = y + 'px';
-
-    container.appendChild(shape);
-}
-// shape numbers
-const numShapes = 100;
-for (let i = 0; i < numShapes; i++) {
-    generateShape();
-}
-
 let link = undefined
 function scrollToSection(link) {
     window.open(link, "_self")
