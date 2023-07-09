2a511345d2c14d63daada584c5919d1f3fc9c0a0
Lucas <garcia.lucas3110@icloudd.com>
2023-07-09 00:33:01 -0400

    updated styles for mobile

--- app.js
+++ app.js
@@ -5,29 +5,6 @@ window.addEventListener('load', function () {
     }, 100);
 });
 
-
-// Responsive hamburger menu functionality
-const hamburger = document.querySelector('.hamburger');
-const responsiveMenu = document.createElement('div');
-responsiveMenu.classList.add('responsive-menu');
-
-// Create responsive menu
-const navItems = document.querySelectorAll('nav ul li');
-navItems.forEach(item => {
-    const link = item.querySelector('a');
-    const clone = link.cloneNode(true);
-    responsiveMenu.appendChild(clone);
-});
-
-// Toggle responsive menu on click
-hamburger.addEventListener('click', () => {
-    if (responsiveMenu.style.display === 'block') {
-        responsiveMenu.style.display = 'none';
-    } else {
-        responsiveMenu.style.display = 'block';
-    }
-});
-
 // Insert responsive menu
 const header = document.querySelector('header');
 header.appendChild(responsiveMenu);
